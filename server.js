a5c2b0aebb74a7a623647a0c20d2e98a45a87bda
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-01-20 18:24:43 +0530

    Based on the url the user hits returning custom responses

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
-const http = require('http');
+const http = require("http");
 
 let server = http.createServer((req, res) => {
-    console.log('Shubham Swarnkar')
+  let url = req.url;
+  if (url == "/node") {
+    res.write("<html>");
+    res.write("<header><title>Node</title></header>");
+    res.write("<body><h2>Welcome to my Node Js project</h2></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url == "/home") {
+    res.write("<html>");
+    res.write("<header><title>Home</title></header>");
+    res.write("<body><h2>Welcome Home</h2></body>");
+    res.write("</html>");
+    return res.end();
+  } else if (url == "/about"){
+    res.write("<html>");
+    res.write("<header><title>About</title></header>");
+    res.write("<body><h2>Welcome to About Us page</h2></body>");
+    res.write("</html>");
+    return res.end();
+  }
 });
 
-server.listen(4000);
+server.listen(3000);
