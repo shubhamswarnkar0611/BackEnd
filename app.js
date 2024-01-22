12805235de03b4782c840ef94ad1f04b2440b848
shubhamswarnkar0611 <iamshubham0611@gmail.com>
2024-01-22 17:29:53 +0530

    Clean Up Code though export and import

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 const http = require("http");
-const fs = require("fs");
+const route= require("./routes");
 
-const server = http.createServer((req, res) => {
-  let url = req.url;
-  let method = req.method;
-
-  if (url === "/") {
-    fs.readFile("message.txt", (err, data) => {
-      res.write("<html>");
-      res.write("<header><title>Node</title></header>");
-      res.write(
-        `<body><p>${data}</p><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`
-      );
-      res.write("</html>");
-      return res.end();
-    });
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunks) => {
-      body.push(chunks);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
- 
-});
+const server = http.createServer(route);
 
 server.listen(3000);
